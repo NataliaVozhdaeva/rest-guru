8f4efd08021b7c77dc08ec9ec0757d9cedd5f5d5
NataliaVozhdaeva <LomzNat@yandex.ru>
2024-04-01 15:15:53 +0400

    feat: add adaptive for slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -4,17 +4,33 @@ import { Navigation, Pagination } from 'swiper/modules';
 Swiper.use([Navigation, Pagination]);
 
 const swiper = new Swiper('.swiper', {
-  slidesPerView: 4,
   navigation: {
     nextEl: '.swiper-btn--next',
     prevEl: '.swiper-btn--prev',
   },
+
   keyboard: {
     enabled: true,
     onlyInViewport: true,
     pageUpDown: true,
   },
+
   spaceBetween: 20,
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    980: {
+      slidesPerView: 3,
+    },
+    1300: {
+      slidesPerView: 4,
+    },
+  },
 });
 
 export default swiper;
